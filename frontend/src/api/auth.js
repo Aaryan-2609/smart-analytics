a5da8538a75b90a02ce2f9009f4016d44d353f51
Aaryan-2609 <aaryankamdar7999@gmail.com>
2026-03-28 22:38:26 +0530

    improve connections

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5002/api';
+const API_BASE_URL = 'https://smart-analytics-backend-l03x.onrender.com';
 
 export const loginUser = async (email, password) => {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
