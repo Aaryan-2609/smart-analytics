d5e7148cc83daf680084080b7c0520bb7b2a179b
Aaryan-2609 <aaryankamdar7999@gmail.com>
2026-04-09 10:46:07 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = 'https://smart-analytics-backend-l03x.onrender.com';
-// const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 export const loginUser = async (email, password) => {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
     method: 'POST',
